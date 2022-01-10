ca06e3ce65ca298723346b529566c9a65749c536
Nwabufor John <johnzeus14@gmail.com>
2022-01-10 19:14:43 +0100

    added Tailwinds CSS

---
+++ src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+
+<nav class="flex justify-between items-center py-4 px-8">
+
+  <div>
+
+    <h1 class="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"> Skillfy</h1>
+
+
+  </div>
+
+    <div>
+
+      <button class ="bg-black text-white py-3 px-3 rounded-lg "> Get started</button>
+
+  </div>
+  
+
+
+
+</nav>
+
+</template>
+
+<script>
+export default {
+  
+
+
+}
+
+</script>
