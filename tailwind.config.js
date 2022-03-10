9fc487d39206ab615e1ffbee978a8919a496ac38
Nwabufor John <johnzeus14@gmail.com>
2022-03-10 20:20:10 +0100

    updated tailwinds to V3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 module.exports = {
 
-  mode: 'jit',
-  purge: [
 
-  './src/App.vue',
-     './src/components/Navbar.vue',
-      './src/components/Footbar.vue',
-    './src/views/Home.vue',
-     './src/views/Login.vue',
-     './src/views/Register.vue',
+  content: [
 
+    "./src/**/*.{html,js,vue}",
+    "./views/**/*.{vue}"
   
 
     ],
